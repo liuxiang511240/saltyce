a9c734904fb5e5f12b92b6ad9ddcee663992a6b1
tony <liuxiang511240@163.com>
2015-08-04 23:50:55 +0800

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Salty::Application.config.secret_key_base = 'c1f0fa9bcb592ca858c96d8a810c8328bd3c07f069fd28c6adbb3fd52979abff7b441ded8736bbe47c31d33687d4da4280c50b37850be2f7a087b859ebc36fc9'
